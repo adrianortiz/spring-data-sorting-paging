f3bdf4ca7bf4c00792441ccb74aa8e2e37e12c9b
Adrian Ortiz <adrianortizmartinez@gmail.com>
2023-05-19 23:58:45 -0600

    Updated JS Function Btn paging

--- src/main/resources/static/js/libros.js
+++ src/main/resources/static/js/libros.js
@@ -6,6 +6,7 @@
         init: function () {
             App.FilterEmptyInput();
             App.OrderBy();
+            App.Paging();
         },
         FilterEmptyInput: function () {
             $("form").submit(function () {
@@ -23,6 +24,19 @@
                 $("form").submit();
             })
         },
+        Paging: function () {
+            $("#btn-paging-back").click(function (e) {
+                if (parseInt($("#pagina").val()) > 0) {
+                    $("#pagina").val(parseInt($("#pagina").val())-1);
+                }
+                $("form").submit();
+            });
+
+            $("#btn-paging-next").click(function (e) {
+                $("#pagina").val(parseInt($("#pagina").val())+1);
+               $("form").submit();
+            });
+        }
     };
 
     $(function(){
