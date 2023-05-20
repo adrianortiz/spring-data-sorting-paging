ff3e665e392dd67fa29a657dac3c215e3a76324e
Adrian Ortiz <adrianortizmartinez@gmail.com>
2023-05-20 02:22:56 -0600

    Update LibroController

--- src/main/java/com/company/springdata/controller/LibroController.java
+++ src/main/java/com/company/springdata/controller/LibroController.java
@@ -40,37 +40,6 @@ public class LibroController {
         return "libros";
     }
 
-    @RequestMapping(value = "buscartodos", params = "titulo")
-    public String buscarTodosPorTitulo(String titulo, Model modelo){
-        Iterable<Libro> libros = repository.findByTitulo(titulo);
-        modelo.addAttribute("libros", libros);
-        return "libros";
-    }
-
-    @RequestMapping(value = "buscartodos", params = "autor")
-    public String buscarTodosPorAutor(String autor, Model modelo) {
-        Iterable<Libro> libros = repository.findByAutor(autor);
-        modelo.addAttribute("libros", libros);
-        return "libros";
-    }
-
-    @RequestMapping(value = "buscartodos", params = {"titulo", "autor"})
-    public String buscarTodosPorTituloyAutor(String titulo, String autor, Model modelo) {
-        Iterable<Libro> libros = repository.findByTituloAndAutor(titulo, autor);
-        modelo.addAttribute("libros", libros);
-        return "libros";
-    }
-
-    /*
-    @RequestMapping(value = "buscartodos", params = {"titulo", "orden"})
-    public String buscarTodosPorTitulo(String titulo, String orden, Model modelo){
-        Iterable<Libro> libros = repository.findByTitulo(titulo, Sort.by(orden));
-        modelo.addAttribute("libros", libros);
-        modelo.addAttribute("titulo", titulo);
-        return "libros";
-    }
-     */
-
     @RequestMapping(value = "buscarTodos", params = {"titulo", "orden", "pagina", "size"})
     public String buscarTodosPorTitulo(String titulo, String orden,
                                        @RequestParam(name = "pagina", defaultValue = "0") int pagina,
@@ -82,21 +51,4 @@ public class LibroController {
         return "libros";
     }
 
-    @RequestMapping(value = "buscartodos", params = {"autor", "orden"})
-    public String buscarTodosPorAutor(String autor, String orden, Model modelo) {
-        Iterable<Libro> libros = repository.findByAutor(autor, Sort.by(orden));
-        modelo.addAttribute("libros", libros);
-        modelo.addAttribute("autor", autor);
-        return "libros";
-    }
-
-    @RequestMapping(value = "buscartodos", params = {"titulo", "autor", "orden"})
-    public String buscarTodosPorTituloyAutor(String titulo, String autor, String orden, Model modelo) {
-        Iterable<Libro> libros = repository.findByTituloAndAutor(titulo, autor, Sort.by(orden));
-        modelo.addAttribute("libros", libros);
-        modelo.addAttribute("titulo", titulo);
-        modelo.addAttribute("autor", autor);
-
-        return "libros";
-    }
 }
