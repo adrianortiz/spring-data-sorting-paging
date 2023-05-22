3c132820a917d0b54406527e0f89b8e09ff4793c
Adrian Ortiz <adrianortizmartinez@gmail.com>
2023-05-21 23:34:13 -0600

    Using Spring Data Rest

--- src/main/java/com/company/springdata/repository/CapituloRepository.java
+++ src/main/java/com/company/springdata/repository/CapituloRepository.java
@@ -2,9 +2,11 @@ package com.company.springdata.repository;
 
 import com.company.springdata.entity.Capitulo;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(path = "capitulos")
 public interface CapituloRepository extends CrudRepository<Capitulo, String> {
 
     List<Capitulo> findByTitulo(String titulo);
